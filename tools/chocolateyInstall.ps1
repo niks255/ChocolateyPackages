529427e3d37e5425f6c157e90365482c96fa22c7
niks255 <niks255@mail.ru>
2016-06-25 01:48:30 +0300

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = "foobar2000"
+$url = 'http://www.foobar2000.org/files/34e98dc3b7f1288baf90bdc512e8412b/foobar2000_v1.3.10.exe' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE' 
+  url           = $url
+  silentArgs    = "/S"
+  validExitCodes= @(0)
+  registryUninstallerKey = 'foobar2000'
+  checksum      = '2994486afdcce7f78d80019a797a8ab2'
+  checksumType  = 'md5'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,28 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'foobar2000'
+$registryUninstallerKeyName = 'foobar2000'
+$uninstallerName = 'uninstall.exe'
+$shouldUninstall = $true
+
+$machine_key   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+$machine_key6432 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName"
+
+$path = @($machine_key, $machine_key6432) `
+    | ?{ Test-Path $_ } `
+    | Get-ItemProperty `
+    | Select-Object -ExpandProperty InstallLocation
+
+$file = $path + '\' + $uninstallerName
+
+if ($file -eq $null -or $file -eq '') {
+    Write-Host "$packageName has already been uninstalled by other means."
+    $shouldUninstall = $false
+}
+
+$installerType = 'exe' 
+$validExitCodes = @(0)
+
+if ($shouldUninstall) {
+ Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file 
+}
