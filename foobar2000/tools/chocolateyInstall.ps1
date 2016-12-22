66121b23c2c4340a1ea336e8a8aa262dd61ac043
niks255 <niks255292@gmail.com>
2016-12-22 21:19:29 +0300

    foobar2000: download from softpedia.com

--- foobar2000/tools/chocolateyInstall.ps1
+++ foobar2000/tools/chocolateyInstall.ps1
@@ -1,39 +1,17 @@
 ﻿$packageName = "foobar2000"
 
-$pwd = "$(split-path -parent $MyInvocation.MyCommand.Definition)"
-Import-Module "$($pwd)\Get-FilenameFromRegex.ps1" -Force
-# Why does an import failure on this module not throw an error?
-
 $ErrorActionPreference = "Stop"
 
-# TODO: Remvoe all this junk and use chocolatey core extension to download installer
-try {
-  $url1 = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/history/" 'download_foobar2000/([\d]+)/"[^>]*>Foobar2000 1.3.13<' 'http://www.filehippo.com/download_foobar2000/$1/'
-} catch {
-  Write-Host "Looking for alternative url"
-  $latestVersion = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/" 'itemprop\=\"name\"\>[\s\r\n]*Foobar2000 (1.3.13)<' '$1'
-  if ($latestVersion -ne '') {
-    Write-Host "Attempting to find url for latest version"
-    $url1 = Get-FilenameFromRegex "http://www.filehippo.com/download_foobar2000/" 'download_foobar2000/download/([\w\d]+)/[^>]*>[\s\r\n]*<span>Download Latest Version<' 'http://www.filehippo.com/en/download_foobar2000/download/$1/'
-  }
-}
-
-Write-Host "Found URL that redirects to the download url: $url1"
-
-$url2 = Get-FilenameFromRegex "$url1" 'download_foobar2000/download/([\w\d]+)/' 'http://www.filehippo.com/en/download_foobar2000/download/$1/'
-Write-Host "Found secondary url that redirects to the download url: $url2"
-
-$url3 = Get-FilenameFromRegex "$url2" '/download/file/([\w\d]+)/' 'http://www.filehippo.com/download/file/$1/'
-Write-Host "Found download URL: $url3"
+$url='https://softpedia-secure-download.com/dl/163d25bae81ec75618fecc6b3f823434/585c175f/100006492/software/multimedia/player/foobar2000_v1.3.14.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'EXE' 
-  url           = $url3
+  url           = $url
   silentArgs    = "/S"
   validExitCodes= @(0)
-  checksum      = '90C7E45C11283B298B9D33A243BDDD91'
+  checksum      = '63454F7A0E69DAB9B3FE2DFC9A2C6FC6'
   checksumType  = 'md5'
 }
 
