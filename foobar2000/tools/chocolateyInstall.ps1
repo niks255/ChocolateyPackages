63de89149ff3bc176ba4926e7d0ee60bf3a7fccd
niks255 <niks255292@gmail.com>
2016-12-29 22:44:35 +0300

    Keep history for a package

--- foobar2000/tools/chocolateyInstall.ps1
+++ foobar2000/tools/chocolateyInstall.ps1
@@ -1,16 +1,16 @@
-﻿$packageName = "foobar2000"
+﻿$packageName = 'foobar2000'
+$ErrorActionPreference = 'Stop'
 
-$ErrorActionPreference = "Stop"
-
-$temp = ((Invoke-WebRequest -Uri 'http://filehippo.com/download_foobar2000/').Links | Where-Object {$_.class -Like "*program-header-download-link green button-link active long download-button*"}).href
-$url = "http://filehippo.com"+((Invoke-WebRequest -Uri $temp ).Links | Where-Object {$_.outerHTML -Like "*id=download-link*"}).href
+$appurl = 'http://filehippo.com'+((Invoke-WebRequest -Uri 'http://filehippo.com/download_foobar2000/history').Links | Where-Object {$_.InnerHTML -NotLike '*Beta*'} | Where {$_.InnerHTML -Match 'Foobar2000 '+$version}).href 
+$temp = ((Invoke-WebRequest -Uri $appurl).Links | Where-Object {$_.class -Like '*program-header-download-link green button-link active long download-button*'}).href
+$url = 'http://filehippo.com'+((Invoke-WebRequest -Uri $temp ).Links | Where-Object {$_.outerHTML -Like '*id=download-link*'}).href
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
   fileType      = 'exe' 
   url           = $url
-  silentArgs    = "/S"
+  silentArgs    = '/S'
   validExitCodes= @(0)
   checksum      = '63454F7A0E69DAB9B3FE2DFC9A2C6FC6'
   checksumType  = 'md5'
