b72c3b50d39473800bc4e11936426e0a6a93094c
niks255 <niks255292@gmail.com>
2017-03-09 21:37:17 +0300

    Update TagScanner to 6.0.20
    Also, use sha256 instead of md5

--- TagScanner/tools/chocolateyinstall.ps1
+++ TagScanner/tools/chocolateyinstall.ps1
@@ -2,15 +2,15 @@
 
 
 $packageName = 'TagScanner' 
-$url = 'http://www.xdlab.ru/files/tagscan-6.0.19-setup.exe' 
+$url = 'http://www.xdlab.ru/files/tagscan-6.0.20-setup.exe' 
 
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE' 
+  fileType      = 'exe' 
   url           = $url
-  checksum      = 'CACEFBA062CECFECDFB1D91FB607337E'
-  checksumType  = 'md5'
+  checksum      = '0FE652E02E077C1D5907ECEAFDE7F5DE1B417EF8E78D0911B86A73645252C2F8'
+  checksumType  = 'sha256'
   silentArgs    = "/VERYSILENT /NORESTART /SP- /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS"
   validExitCodes= @(0)
 }
