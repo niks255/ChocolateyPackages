16852897e9963ea8df085c36606a8e9581807ffe
niks255 <niks255292@gmail.com>
2017-03-18 16:52:03 +0300

    Add TagScanner portable

---
+++ TagScanner Portable/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'TagScanner.portable' 
+$binarypath = "$([Environment]::GetFolderPath('ApplicationData'))\\TagScanner Portable"
+$url = 'https://www.xdlab.ru/files/tagscan-6.0.20.zip' 
+
+$packageArgs = @{
+  packageName   = $packageName
+  UnzipLocation = $binarypath
+  url           = $url
+  checksum      = '24A8F27F6238B30BEE44ADCA31A28F60B23C9922422CF23311D7B39762173364'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut -ShortcutFilePath "$([Environment]::GetFolderPath('Desktop'))\\TagScanner Portable.lnk" -TargetPath "$binarypath\\Tagscan.exe"
---
+++ TagScanner Portable/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop';
+$binarypath = "$([Environment]::GetFolderPath('ApplicationData'))\\TagScanner Portable"
+
+try {
+    Remove-Item $binarypath -recurse
+} catch {
+    Write-host("Package folder has already been removed by other means, skipping...")
+}
+
+try {
+    Remove-Item "$([Environment]::GetFolderPath('Desktop'))\\TagScanner Portable.lnk"
+} catch {
+    Write-host("Desktop shortcut couldn't be removed, skipping...")
+}
