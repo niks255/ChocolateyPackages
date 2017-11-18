69d0fc5bb71164c99132ab08fb26e1c21c2a19f6
niks255 <niks255292@gmail.com>
2017-11-18 17:11:05 +0300

    Fix uninstall script for foobar2000

--- foobar2000/tools/chocolateyuninstall.ps1
+++ foobar2000/tools/chocolateyuninstall.ps1
@@ -2,13 +2,25 @@
  
 $packageName = 'foobar2000'
 
-$key = Get-UninstallRegistryKey $packageName
-$file = $key.InstallLocation + '\' + 'uninstall.exe'
+[array]$key = Get-UninstallRegistryKey -SoftwareName "foobar2000*"
 
-$installerType = 'exe'
-$silentArgs = '/S'
-$validExitCodes = @(0)
-
-if ($file -ne $null -and $file -ne '') {
-    Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
+if ($key.Count -eq 1) {
+  $key | % {
+    $packageArgs = @{
+      packageName = $packageName
+      fileType    = 'exe'
+      silentArgs  = '/S'
+      validExitCodes= @(0)
+      file          = $key.InstallLocation + "\" + "uninstall.exe"
+    }
+ 
+    Uninstall-ChocolateyPackage @packageArgs
+  }
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
 }
