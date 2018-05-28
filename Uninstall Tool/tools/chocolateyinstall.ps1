bb91624c18bf6c3664182e360f1ee5f0b305c6c0
niks255 <niks255292@gmail.com>
2018-05-28 15:51:14 +0300

    Fix Uninstall Tool download link

--- Uninstall Tool/tools/chocolateyinstall.ps1
+++ Uninstall Tool/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 
 $packageName = 'UninstallTool'
-$url = 'https://www.crystalidea.com/downloads/uninstalltool_setup.exe'
+$url = 'https://github.com/crystalidea/uninstall-tool/releases/download/v3.5.5/uninstalltool_setup.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'EXE'
   url           = $url
-  checksum      = '3f9949e62032179b1f93d36cbf010798f9f8be5847b749f922ee93883cf896cc'
+  checksum      = '3d57a1fbc5236d1e7105de54a1a2e0531c2a2a07188df05583f9ccdd6819bfa0'
   checksumType  = 'sha256'
   silentArgs    = "/verysilent /norestart"
   validExitCodes= @(0, 3010, 1641)
