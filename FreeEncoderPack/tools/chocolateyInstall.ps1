a6c9905139b23913171a751b7d65cc900b8522a0
niks255 <niks255292@gmail.com>
2018-06-23 16:24:27 +0300

    Add FreeEncoderPack

---
+++ FreeEncoderPack/tools/chocolateyInstall.ps1
@@ -0,0 +1,14 @@
+﻿$packageName = 'freeencoderpack'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$ErrorActionPreference = 'Stop'
+ 
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe' 
+  file          = Join-Path $toolsDir 'Free_Encoder_Pack_2018-05-22.exe'
+  silentArgs    = '/S'
+  validExitCodes= @(0)
+}
+
+Install-ChocolateyInstallPackage @packageArgs
