b8b9713405dedf9ca5900f563a567ca7c0a99a78
niks255 <niks255292@gmail.com>
2019-06-24 22:07:23 +0300

    Update Uninstall Tool to 3.5.8

--- Uninstall Tool/tools/chocolateyinstall.ps1
+++ Uninstall Tool/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 
 $packageName = 'UninstallTool'
-$url = 'https://github.com/crystalidea/uninstall-tool/releases/download/v3.5.7/uninstalltool_setup.exe'
+$url = 'https://www.crystalidea.com/downloads/uninstalltool_setup.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   fileType      = 'EXE'
   url           = $url
-  checksum      = '6AA7927BD9D605174156E6F28421D557FADD112523BC5BB04F2BB699A1D8223F'
+  checksum      = '3D8D45204ABFD27BBF71190D1742DC17EC2BC5008A552315A18ED8BDB8AD4200'
   checksumType  = 'sha256'
   silentArgs    = "/verysilent /norestart"
   validExitCodes= @(0, 3010, 1641)
