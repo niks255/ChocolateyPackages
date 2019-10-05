45d5f3d939528033f59bcabd82056c10f9295122
TheCakeIsNaOH <38865330+TheCakeIsNaOH@users.noreply.github.com>
2019-10-05 10:46:00 -0500

    Foobar2000 add parameter for no desktop icon

--- foobar2000/tools/chocolateyInstall.ps1
+++ foobar2000/tools/chocolateyInstall.ps1
@@ -1,6 +1,9 @@
 ﻿$packageName = 'foobar2000'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $ErrorActionPreference = 'Stop'
+$pp                    = Get-PackageParameters
+$shortcutName          = 'foobar2000.lnk'
+$shortcut              = Join-Path ([Environment]::GetFolderPath("CommonDesktopDirectory")) $shortcutName
  
 $packageArgs = @{
   packageName   = $packageName
@@ -12,3 +15,12 @@ $packageArgs = @{
 }
 
 Install-ChocolateyInstallPackage @packageArgs
+
+if ($pp['noicon']) {
+	if (Test-Path $shortcut) {
+		Remove-Item $shortcut
+		Write-Host -ForegroundColor green 'Removed ' $shortcut
+	} else {
+		Write-Host -ForegroundColor yellow 'Did not find ' $shortcut
+	}
+}
