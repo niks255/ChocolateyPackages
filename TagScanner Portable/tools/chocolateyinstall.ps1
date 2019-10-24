1d780f41fa7fc34e6812e02052df8efbb1678a1b
niks255 <niks255292@gmail.com>
2019-10-24 20:57:32 +0300

    Update TagScanner to 6.1.0

--- TagScanner Portable/tools/chocolateyinstall.ps1
+++ TagScanner Portable/tools/chocolateyinstall.ps1
@@ -2,14 +2,17 @@
 
 $packageName = 'TagScanner.portable' 
 $binarypath = "$([Environment]::GetFolderPath('ApplicationData'))\\TagScanner Portable"
-$url = 'https://www.xdlab.ru/files/tagscan61beta.zip' 
+$url = 'https://www.xdlab.ru/files/tagscan-6.1.0.zip'
+$url64 = "https://www.xdlab.ru/files/tagscan-6.1.0_x64.zip"
 
 $packageArgs = @{
-  packageName   = $packageName
-  UnzipLocation = $binarypath
-  url           = $url
-  checksum      = '2ffba90f51ea73387c79882fb0668d9a3464206f43ada69af8c11861eed94cdc'
-  checksumType  = 'sha256'
+  packageName       = $packageName
+  UnzipLocation     = $binarypath
+  url               = $url
+  checksum          = '51f226fe5e8bd3656fcd9486e4834c2c0873c771c0838cb31fa551ebd951c302'
+  checksum64        = '6b53c2873b2f80c1bd655a2df22910a58fe6519635f1acd11e45c2f4a63212df'
+  checksumType      = 'sha256'
+  checksumType64    = 'sha256'
 }
 
 Install-ChocolateyZipPackage @packageArgs
