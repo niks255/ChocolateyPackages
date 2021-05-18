5844e95f96ae909d7b48f35a2a897356a677ec20
niks255 <niks255292@gmail.com>
2021-05-18 21:27:38 +0300

    Update packages

--- librespeed-cli/tools/chocolateyInstall.ps1
+++ librespeed-cli/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $ToolsPath = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     UnzipLocation  = "$ToolsPath"
-    url            = 'https://github.com/librespeed/speedtest-cli/releases/download/v1.0.8/librespeed-cli_1.0.8_windows_386.zip'
-    url64          = 'https://github.com/librespeed/speedtest-cli/releases/download/v1.0.8/librespeed-cli_1.0.8_windows_amd64.zip'
-    checksum       = 'b760539d7927b140af173e05e2ca2d534705158282065a8730ad1fa9b2fc0f4b'
-    checksum64     = '3b28996d49610a3e3d0c66694a32df49625352e2c7833b93a04679db3af9f471'
+    url            = 'https://github.com/librespeed/speedtest-cli/releases/download/v1.0.9/librespeed-cli_1.0.9_windows_386.zip'
+    url64          = 'https://github.com/librespeed/speedtest-cli/releases/download/v1.0.9/librespeed-cli_1.0.9_windows_amd64.zip'
+    checksum       = 'd9af11c068d20f6c379c0b5b92c20ff0deda3092dc48dbb6789c8a09374ddf6f'
+    checksum64     = '82838e03cb61eb12f4af5ed1140a62e800ba7848222a31d457a95760dd08ebf7'
     checksumtype   = 'sha256'
     checksumtype64 = $checksumtype
 }
