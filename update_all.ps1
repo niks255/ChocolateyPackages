288cf963d65036f3b7f843b903e151fced990eb6
niks255 <niks255292@gmail.com>
2021-06-07 21:42:15 +0300

    Use git plugin

--- update_all.ps1
+++ update_all.ps1
@@ -59,7 +59,12 @@ $Options = [ordered]@{
         Exclude = 'password', 'apikey', 'apitoken'          #Option keys which contain those words will be removed
         Path    = "$PSScriptRoot\update_info.xml"           #Path where to save the run info
     }
-    
+
+    Git = @{
+        User     = 'niks255'                                #Git username, leave empty if github api key is used
+        Password = $Env:github_api_key                      #Password if username is not empty, otherwise api key
+    }
+
     Telegram = @{
         token      = $Env:tg_token
         chat_id    = $Env:tg_chat_id
