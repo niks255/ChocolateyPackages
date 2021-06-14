30ade2df45cc16193621c4f40422dca50de05b5a
niks255 <niks255292@gmail.com>
2021-06-14 22:09:46 +0300

    EverythingToolbar: only support 64-bit systems
    https://github.com/stnkl/EverythingToolbar/issues/90

--- everythingtoolbar/tools/chocolateyInstall.ps1
+++ everythingtoolbar/tools/chocolateyInstall.ps1
@@ -3,9 +3,9 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url            = 'https://github.com/stnkl/EverythingToolbar/releases/download/0.6.3/EverythingToolbar-0.6.3.msi'
-  checksum       = '55f33b29e9a7b4c50729d96f8826d7def44d112f5342daa8a8f3db6cdb8e97ad'
-  checksumType   = 'sha256'
+  url64          = 'https://github.com/stnkl/EverythingToolbar/releases/download/0.6.3/EverythingToolbar-0.6.3.msi'
+  checksum64     = '55f33b29e9a7b4c50729d96f8826d7def44d112f5342daa8a8f3db6cdb8e97ad'
+  checksumtype64 = 'sha256'
   SilentArgs     = "/qn /norestart"
   ValidExitCodes = @(0, 3010, 1641)
 }
--- everythingtoolbar/update.ps1
+++ everythingtoolbar/update.ps1
@@ -3,8 +3,8 @@
 function global:au_SearchReplace {
    @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*url\s*=\s*)('.*')"          = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum32)'"
+            "(?i)(^\s*url64\s*=\s*)('.*')"          = "`$1'$($Latest.URL64)'"
+            "(?i)(^\s*checksum64\s*=\s*)('.*')"     = "`$1'$($Latest.Checksum64)'"
         }
     }
 }
@@ -13,7 +13,7 @@ function global:au_GetLatest {
     Get-GithubRepoInfo -User 'stnkl' -Repo 'EverythingToolbar'
 
     @{
-        URL32 = $($links -match '.msi').browser_download_url
+        URL64 = $($links -match '.msi').browser_download_url
         Version = $tag
     }
 }
