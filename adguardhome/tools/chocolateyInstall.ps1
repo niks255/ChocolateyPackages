bdc5457c87606b5a7f8fda598b1e44ed25c2949e
niks255 <niks255292@gmail.com>
2023-03-10 05:41:45 +0300

    AU: 1 updated - adguardhome [skip ci]

--- adguardhome/tools/chocolateyInstall.ps1
+++ adguardhome/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     UnzipLocation  = "$(Get-ToolsLocation)"
-    url            = 'https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.25/AdGuardHome_windows_386.zip'
-    url64          = 'https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.25/AdGuardHome_windows_amd64.zip'
-    checksum       = 'c9796f505f4201696b4c37e8eee4eb211cbdc0ebf61f7ed02b950a935a09b6b0'
-    checksum64     = '2d5ec167a3967f42ac1b5782b66a089fc3b2557c97a359a0ca3d8031feefbb5a'
+    url            = 'https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.26/AdGuardHome_windows_386.zip'
+    url64          = 'https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.107.26/AdGuardHome_windows_amd64.zip'
+    checksum       = '4d1f14856cb2cdf3aab9a1a3371abe6d1d3fe198d0b844a4c96649728a685a39'
+    checksum64     = 'eeee3b175f34f5c66ae61e0f1fb2b467d6d3095b19934567571eff70e3c5fac2'
     checksumtype   = 'sha256'
     checksumtype64 = 'sha256'
 }
