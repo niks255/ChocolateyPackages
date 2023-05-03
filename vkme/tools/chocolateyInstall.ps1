52d75d4aeeba6d73b93ef968e51c22f680cc9ded
niks255 <niks255292@gmail.com>
2023-05-04 02:39:03 +0300

    AU: 1 updated - vkme [skip ci]

--- vkme/tools/chocolateyInstall.ps1
+++ vkme/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName       = $env:ChocolateyPackageName
   fileType          = 'EXE'
-  url64             = 'https://upload.object2.vk-apps.com/vk-me-desktop-dev-5837a06d-5f28-484a-ac22-045903cb1b1a/6.0.5/vk-messenger-6.0.5-build.16-x64.exe'
-  checksum64        = '6ab523d498887cd9865c1ddcd3c2cc6303bfc2ec038f0399b1b962b811ad4105'
+  url64             = 'https://upload.object2.vk-apps.com/vk-me-desktop-dev-5837a06d-5f28-484a-ac22-045903cb1b1a/6.0.7/vk-messenger-6.0.7-build.19-x64.exe'
+  checksum64        = '0661245b066cbd18045239716dbd70f430fb867df2482a734ce172b6c70b4d3c'
   checksumType64    = 'sha256'
   silentArgs        = '/S'
   validExitCodes= @(0)
