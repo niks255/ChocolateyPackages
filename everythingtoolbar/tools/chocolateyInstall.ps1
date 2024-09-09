a0b02364fc2e29945053c5c1b0c4b050668c1939
niks255 <niks255292@gmail.com>
2024-09-09 12:00:51 +0300

    AU: 1 updated - everythingtoolbar [skip ci]

--- everythingtoolbar/tools/chocolateyInstall.ps1
+++ everythingtoolbar/tools/chocolateyInstall.ps1
@@ -3,8 +3,8 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'msi'
-  url64          = 'https://github.com/srwi/EverythingToolbar/releases/download/1.4.0/EverythingToolbar-1.4.0.msi'
-  checksum64     = '84bd2ab86d5ad95859ecdc5be867a85e344ecf1d791b380ed04381da32fbde47'
+  url64          = 'https://github.com/srwi/EverythingToolbar/releases/download/1.4.1/EverythingToolbar-1.4.1.msi'
+  checksum64     = 'e3b177af10305253c19486560b97bde36ae961f1475f308170641c0eb2b1f49c'
   checksumtype64 = 'sha256'
   SilentArgs     = "/qn /norestart"
   ValidExitCodes = @(0, 3010, 1641)
