432344256085ff0cd8d49e1defa97bf96843e683
niks255 <niks255292@gmail.com>
2025-05-11 12:00:50 +0300

    AU: 1 updated - tagscanner.portable [skip ci]

--- tagscanner.portable/tools/chocolateyInstall.ps1
+++ tagscanner.portable/tools/chocolateyInstall.ps1
@@ -5,10 +5,10 @@ $InstallPath = "$([Environment]::GetFolderPath('ApplicationData'))\\TagScanner P
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     UnzipLocation  = $InstallPath
-    url            = 'https://www.xdlab.ru/files/tagscan-6.1.19.zip'
-    url64          = 'https://www.xdlab.ru/files/tagscan-6.1.19_x64.zip'
-    checksum       = '10f434bd1a7a61f1874457d852ee8d826a938f7852f85ccd19e5200829ba8120'
-    checksum64     = '9762bc33584b373a214cbb891aad219803901bf1edf51f37538a54365fbca6bb'
+    url            = 'https://www.xdlab.ru/files/tagscan-6.1.20.zip'
+    url64          = 'https://www.xdlab.ru/files/tagscan-6.1.20_x64.zip'
+    checksum       = '1bb4bf042c2f5cf4316a089cb754e81c3dac82253c1a3f6510b64e8b786b917f'
+    checksum64     = '888a8f228a36accf2f7793b991c0260d36cea5a3489405431b7a051c7fcd474d'
     checksumtype   = 'sha256'
     checksumtype64 = 'sha256'
 }
